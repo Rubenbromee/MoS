5cb20abc473718a7bafa2f50f7083ef62e2f7d4c
Rubenbromee <rubbr232@student.liu.se>
2021-02-25 13:36:37 +0100

    Ok it works now

--- MoS_test.m
+++ MoS_test.m
@@ -1,30 +1,32 @@
 delta_x = 0.1; % Spatial sampling step
-v = 343; % Wave velocity
+v = 343; % Wave velocity for sound in room temperature air
 m = 101; % Room length
-n = 101; % Room width
+n = 201; % Room width
 origo_m = ceil(m/2); % Length midpoint
 origo_n = ceil(n/2); % Width midpoint
 f = 100; % Sound frequency
 n_t = 501; % Number of time steps
 delta_t = 0.0001; % Time step
-c = v * (delta_t / delta_x); 
-t(:, 1) = (0:n_t-1) * delta_t; % Temporal visualization vector
+c = v * (delta_t / delta_x); % Sound constant
 P = zeros(n_t, m, n); % Room visualization matrix
 
-for i = 1:2
-P(i, origo_m, origo_n) = 1;
-P(i, origo_m + 1, origo_n + 1) = 1;
-P(i, origo_m - 1, origo_n + 1) = 1;
-P(i, origo_m - 1, origo_n - 1) = 1;
-P(i, origo_m + 1, origo_n - 1) = 1;
-P(i, origo_m + 1, origo_n) = 1; 
-P(i, origo_m - 1, origo_n) = 1;
-P(i, origo_m, origo_n + 1) = 1;
-P(i, origo_m, origo_n - 1) = 1;
-end
+% Placement of sound source.
+% origo_m = 100;
+% origo_n = 200;
+
+% Starting conditions, impulse source.
+P(1, origo_m, origo_n) = 1;
+P(1, origo_m + 1, origo_n + 1) = 1;
+P(1, origo_m - 1, origo_n + 1) = 1;
+P(1, origo_m - 1, origo_n - 1) = 1;
+P(1, origo_m + 1, origo_n - 1) = 1;
+P(1, origo_m + 1, origo_n) = 1; 
+P(1, origo_m - 1, origo_n) = 1;
+P(1, origo_m, origo_n + 1) = 1;
+P(1, origo_m, origo_n - 1) = 1;
 
 % Accoustic wave equation implemented numerically
-for j = 3:n_t-1 % Iteration over time
+for j = 2:n_t-1 % Iteration over time
     for k = 2:m-1 % Iteration over length
         for l = 2:n-1 % Iteration over width
             P(j+1, k, l) = c^2 * ... 
@@ -36,8 +38,7 @@ for j = 3:n_t-1 % Iteration over time
 end
 
 % Animation and video write loop
-movie_obj = VideoWriter('wave_prop.mj2', 'Motion JPEG AVI');
-movie_obj.Quality = 95;
+movie_obj = VideoWriter('wave_prop.mj2');
 open(movie_obj);
 fig = figure(1);
 for i = 1:n_t
