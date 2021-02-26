3824fcb99c36698e9288b0e595fef1fb40bccc14
Rubenbromee <rubbr232@student.liu.se>
2021-02-26 15:19:26 +0100

    Frekvensimpulser

--- MATLAB/MoS_test.m
+++ MATLAB/MoS_test.m
@@ -4,13 +4,15 @@ m = 101; % Room length
 n = 201; % Room width
 origo_m = ceil(m/2); % Length midpoint
 origo_n = ceil(n/2); % Width midpoint
-f = 100; % Sound frequency
+f = 500; % Sound frequency
 n_t = 501; % Number of time steps
 delta_t = 0.0001; % Time step
 c = v * (delta_t / delta_x); % Sound constant
 P = zeros(n_t, m, n); % Room visualization matrix
+period = 1 / f; % Wave time period
+period_n_t = round(period / delta_t); % Wave time period expressed in amount of time steps
 
-% Placement of sound source.
+% Placement of sound source. Orgio of room as default.
 % origo_m = 100;
 % origo_n = 200;
 
@@ -27,6 +29,18 @@ P(1, origo_m, origo_n - 1) = 1;
 
 % Accoustic wave equation implemented numerically
 for j = 2:n_t-1 % Iteration over time
+    % Create new impulse according to frequency
+    if mod(j, period_n_t) == 0
+        P(j, origo_m, origo_n) = 1;
+        P(j, origo_m + 1, origo_n + 1) = 1;
+        P(j, origo_m - 1, origo_n + 1) = 1;
+        P(j, origo_m - 1, origo_n - 1) = 1;
+        P(j, origo_m + 1, origo_n - 1) = 1;
+        P(j, origo_m + 1, origo_n) = 1; 
+        P(j, origo_m - 1, origo_n) = 1;
+        P(j, origo_m, origo_n + 1) = 1;
+        P(j, origo_m, origo_n - 1) = 1;
+   end
     for k = 2:m-1 % Iteration over length
         for l = 2:n-1 % Iteration over width
             P(j+1, k, l) = c^2 * ... 
@@ -38,7 +52,7 @@ for j = 2:n_t-1 % Iteration over time
 end
 
 % Animation and video write loop
-movie_obj = VideoWriter('wave_prop.mj2');
+movie_obj = VideoWriter('wave_prop_impulse_flashing.avi');
 open(movie_obj);
 fig = figure(1);
 for i = 1:n_t
