f3a98179c076965dd37ccf3b0c7a25ceaa57f2ac
Ruben Bromée <tharubb1992@gmail.com>
2022-02-08 16:23:52 +0100

    Cleanup

--- MoS_test.m
+++ MoS_test.m
@@ -1,18 +1,21 @@
+clear;
 delta_x = 0.1; % Spatial sampling step
 v = 343; % Wave velocity for sound in room temperature air
 m = 101; % Room length
-n = 101; % Room width
+n = 201; % Room width
 origo_m = ceil(m/2); % Length midpoint
 origo_n = ceil(n/2); % Width midpoint
-f = 100; % Sound frequency
-n_t = 501; % Number of time steps
+f = 500; % Sound frequency, Hz
+n_t = 1001; % Number of time steps
 delta_t = 0.0001; % Time step
 c = v * (delta_t / delta_x); % Sound constant
 P = zeros(n_t, m, n); % Room visualization matrix
+period = 1 / f; % Wave time period
+period_n_t = round(period / delta_t); % Wave time period expressed in amount of time steps
 
-% Placement of sound source.
-% origo_m = 100;
-% origo_n = 200;
+% Placement of sound source. Orgio of room as default.
+% origo_m = 50;
+% origo_n = 150;
 
 % Starting conditions, impulse source.
 P(1, origo_m, origo_n) = 1;
@@ -27,6 +30,18 @@ P(1, origo_m, origo_n - 1) = 1;
 
 % Accoustic wave equation implemented numerically
 for j = 2:n_t-1 % Iteration over time
+    % Create new impulse according to frequency
+    if mod(j, period_n_t) == 0
+        P(j, origo_m, origo_n) = 1;
+        P(j, origo_m + 1, origo_n + 1) = 1;
+        P(j, origo_m - 1, origo_n + 1) = 1;
+        P(j, origo_m - 1, origo_n - 1) = 1;
+        P(j, origo_m + 1, origo_n - 1) = 1;
+        P(j, origo_m + 1, origo_n) = 1; 
+        P(j, origo_m - 1, origo_n) = 1;
+        P(j, origo_m, origo_n + 1) = 1;
+        P(j, origo_m, origo_n - 1) = 1;
+   end
     for k = 2:m-1 % Iteration over length
         for l = 2:n-1 % Iteration over width
             P(j+1, k, l) = c^2 * ... 
@@ -38,20 +53,17 @@ for j = 2:n_t-1 % Iteration over time
 end
 
 % Animation and video write loop
-movie_obj = VideoWriter('wave_prop.mj2');
+movie_obj = VideoWriter('wave_prop.avi');
 open(movie_obj);
 fig = figure(1);
 for i = 1:n_t
     i_p(:, :) = P(i , :, :);
     s = abs(i_p(:,:));
     imshow(s(:, :), 'InitialMagnification', 600);
+    colormap default;
     drawnow;
     F = getframe(fig);
     writeVideo(movie_obj, F);
 end
-close(movie_obj);
 
-json_info = jsonencode(P(:,:,:));
-json_file = fopen('jason.json', 'w');
-fwrite(json_file, json_info, 'char');
-fclose(json_file);
+close(movie_obj);
---
+++ README.md
@@ -0,0 +1,6 @@
+A matlab script which models and visualizes the propagation of sound in a room using the acoustic wave equation. It writes the visualization to a video file.
+
+How to run:
+Create a video file by running the script. 
+Change some of the parameters like the frequency or the position if you like. 
+ATTENTION: While running the draw-window flickers a lot. You can minimize the figure window while it's drawing.
